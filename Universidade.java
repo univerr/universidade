1a46e3e02f5e575f7699c39931db34d406a98d4d
heydelailah <heydelailah@gmail.com>
2017-04-03 21:13:33 +0000

    6:17PM 04_03_2017

--- Universidade.java
+++ Universidade.java
@@ -1,12 +1,27 @@
+//developed by alexsyfk..github,ill db implements mysql
+/*
+ * simple app to show functions with db....
+ * docents Fred Jarsen
+ */
 public class Universidade{
     
     public static void main(String args[]){
         
-       System.out.println("hello"); 
+       System.out.println(" ___________________________"); 
+       System.out.println("+\t           \t    +"); 
+       System.out.println("+\t1-consultar\t    +"); 
+       System.out.println("+\t2-cadastrar\t    +"); 
+       System.out.println("+\t3-remover  \t    +"); 
+       System.out.println("+\t\t\t    +"); 
+       System.out.println("+\t\t\t    +"); 
+       System.out.println("+\t\t\t    +"); 
+       System.out.println("+\t\t\t    +"); 
+       System.out.println("+\t\t\t    +"); 
+       System.out.println("+____________________________"); 
+
         
     }
     
     
     
 }
-//developed by alexsyfk
