342aa9ea40a777abcb06811e95fd883b6e77189e
heydelailah <heydelailah@gmail.com>
2017-04-03 21:51:01 +0000

    6:55pm 04_03_2017

--- Universidade.java
+++ Universidade.java
@@ -3,21 +3,21 @@
  * simple app to show functions with db....
  * docents Fred Jarsen
  */
+import java.util.Scanner;
 public class Universidade{
     
     public static void main(String args[]){
         
-       System.out.println(" ___________________________"); 
-       System.out.println("+\t           \t    +"); 
-       System.out.println("+\t1-consultar\t    +"); 
-       System.out.println("+\t2-cadastrar\t    +"); 
-       System.out.println("+\t3-remover  \t    +"); 
-       System.out.println("+\t\t\t    +"); 
-       System.out.println("+\t\t\t    +"); 
-       System.out.println("+\t\t\t    +"); 
-       System.out.println("+\t\t\t    +"); 
-       System.out.println("+\t\t\t    +"); 
-       System.out.println("+____________________________"); 
+       System.out.println(" _________________________________"); 
+       System.out.println("+\t           \t      \t  +\t   projeto db"); 
+       System.out.println("+\t1-consultar\tcriado\t  +\tdocente Fred Jarsen"); 
+       System.out.println("+\t2-cadastrar\t por  \t  +        |"); 
+       System.out.println("+\t3-remover  \talexsy\t  +        |"); 
+       System.out.println("+\t4-atualuzar\t      \t  +        |");
+       System.out.println("+\t                   \t  +        ▼"); 
+       System.out.println("+\t\t\t    \t  +");Scanner sc = new Scanner(System.in);
+       System.out.println("+\t\t\t    \t  +escolha uma das opções..."); 
+       System.out.println("+_________________________________+");sc.nextInt(); 
 
         
     }
